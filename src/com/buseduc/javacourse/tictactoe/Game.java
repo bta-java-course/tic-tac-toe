9e4f12038a38017392a47f34dbb4c396698af43b
Vadim Semenov <vsemenov@leongaming.com>
2019-11-18 17:59:33 +0200

    Tic Tac TOe commint

---
+++ src/com/buseduc/javacourse/tictactoe/Game.java
@@ -0,0 +1,31 @@
+package com.buseduc.javacourse.tictactoe;
+
+import com.buseduc.javacourse.tictactoe.artifacts.Board;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+
+        Board gameBoard = new Board();
+        String cell = getCell();
+        gameBoard.setChip(cell);
+        gameBoard.render();
+
+        
+    }
+    public static String getCell() {
+        Scanner scanner = new Scanner(System.in);
+        String hint = "Введите адрес клетки: ";
+        String cell = "";
+        System.out.println(hint);
+        if (scanner.hasNext()) {
+            cell = scanner.next();
+        }
+        scanner.close();
+        return cell;
+    }
+
+}
